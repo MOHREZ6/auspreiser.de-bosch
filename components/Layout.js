7de932881bc0bb39e7eb5678e476f4d648c7fbed
mohammad.ab95 <mohammadreza.ab1895@gmail.com>
2023-11-02 14:58:20 +0330

    Start

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+
+function Home(){
+    return(
+        <>
+        <div>
+            <header>haeaer</header>
+            <main>main</main>
+            <footer>footer</footer>
+        </div>
+        </>
+    )
+}
+
+
+export default Home
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import Layout from '../components/Layout'
+function Home(){
+    return<Layout></Layout>
+}
+export default Home
