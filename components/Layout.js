4edfc79b43d0d644655b6c3ac4e334bbc88bd21a
mohammad.ab95 <mohammadreza.ab1895@gmail.com>
2023-11-06 16:24:38 +0330

    step2

--- components/Layout.js
+++ components/Layout.js
@@ -30,7 +30,7 @@ function Layout({ title, children }) {
                 <p className="Source Title">Preisvergleiche & produktsuche</p>
               </div>
             </div>
-            <div className="d-flex justify-content-center searchBox  d-flex bd-highlight me-5 p-4   rounded-start">
+            <div className="d-flex justify-content-center searchBox  d-flex bd-highlight me-1 p-4   rounded-start">
               <span
                 className="input-group-text border-0 search-icon rounded-0"
                 id="search-addon"
@@ -51,28 +51,66 @@ function Layout({ title, children }) {
               </span>
               <input
                 type="search"
-                className="form-control rounded-0 rounded-end"
+                className="form-control rounded-0 rounded-end "
                 placeholder="Search"
                 aria-label="Search"
-                 
               />
             </div>
-            <div className="lala d-flex bd-highlight">
-           
-<div class="btn-group">
-  <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    Action
-  </button>
-  <ul class="dropdown-menu">
-    <li><a className="dropdown-item" href="#">Action</a></li>
-    <li><a className="dropdown-item" href="#">Another action</a></li>
-    <li><a className="dropdown-item" href="#">Something else here</a></li>
-    <li    className="dropdown-divider"></li>
-    <li><a className="dropdown-item" href="#">Separated link</a></li>
-  </ul>
-</div>
-
-              <h1>Kategorien</h1>
+            <div className="d-flex align-items-center">
+              {/* <div class="dropdown" > 
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="60"
+                  height="60"
+                  viewBox="0 0 30 30"
+                >
+                  <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
+                </svg>
+                <div class="dropdown-content">
+                  <p>Hello World!</p>
+                 
+                  <p>Hel 
+                </div>
+              </div> */}
+              <div class="dropdown">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  className="me-3   "
+                  width="40"
+                  height="40"
+                  viewBox="0 0 30 30"
+                >
+                  <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
+                </svg>
+                <div class="dropdown-content d-flex p-3 ">
+                  <div class="">
+                    <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
+                      The current button
+                    </button>
+                    <button type="button" class="list-group-item list-group-item-action">A second item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+                  </div>
+                  
+                  <div class=" me-3 ">
+                    <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
+                      The current button
+                    </button>
+                    <button type="button" class="list-group-item list-group-item-action">A second item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+                    <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
+                  </div>
+                  
+                </div>
+              </div>
+              <div className="lala d-flex bd-highlight">
+                <p1 className="fs-4  ">Kategorien</p1>
+              </div>
             </div>
           </div>
         </header>
